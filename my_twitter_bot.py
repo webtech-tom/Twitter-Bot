73a2ff0e0b0e838d9ee3d4dd30bbc0b1a1cc2010
webtech-tom <tom.reisinger@mailbox.org>
2018-12-11 19:44:01 +0100

    Init Commit

---
+++ my_twitter_bot.py
@@ -0,0 +1,11 @@
+import tweepy
+print('test')
+
+CONSUMER_KEY = 'Ddkqk4550f8aH0R7UxX3W3HLF'
+CONSUMER_SECRET = 'wmMj0lahzYYep2j40XzfSBYDMCPEKeQgGCEal70eDwFCOqVhdt'
+ACCESS_KEY = '927618808440901632-fbHTSzKxKscM7FZnBtcGKju4iy4RwzZ'
+ACCESS_SECRET = 'hgPoEKEEdDeiWtEMEPmwmWgZR9TqCYJ2b3JqMah06yItY'
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
