d340d2d533a88d1f6d1f265e988a7efb66944eec
webtech-tom <tom.reisinger@mailbox.org>
2018-12-15 11:57:03 +0100

    mentions added

--- my_twitter_bot.py
+++ my_twitter_bot.py
@@ -9,3 +9,9 @@ ACCESS_SECRET = 'hgPoEKEEdDeiWtEMEPmwmWgZR9TqCYJ2b3JqMah06yItY'
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
+
+
+mentions = api.mentions_timeline()
+
+for mention in mentions:
+    print(str(mention.id) + ' - ' + mention.text)
