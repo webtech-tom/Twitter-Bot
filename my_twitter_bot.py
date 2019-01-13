0d0bc4155d0806d69956cecb1e1df57ceff43894
webtech-tom <tom.reisinger@mailbox.org>
2019-01-13 17:20:47 +0100

    check and send reply every 15 sec added

--- my_twitter_bot.py
+++ my_twitter_bot.py
@@ -1,17 +1,49 @@
 import tweepy
-print('test')
+import time
+from keys import *
 
-CONSUMER_KEY = 'Ddkqk4550f8aH0R7UxX3W3HLF'
-CONSUMER_SECRET = 'wmMj0lahzYYep2j40XzfSBYDMCPEKeQgGCEal70eDwFCOqVhdt'
-ACCESS_KEY = '927618808440901632-fbHTSzKxKscM7FZnBtcGKju4iy4RwzZ'
-ACCESS_SECRET = 'hgPoEKEEdDeiWtEMEPmwmWgZR9TqCYJ2b3JqMah06yItY'
+print('test')
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
+FILE_NAME = 'last_seen_id.txt'
 
-mentions = api.mentions_timeline()
 
-for mention in mentions:
-    print(str(mention.id) + ' - ' + mention.text)
+def retrieve_last_seen_id(file_name):
+    f_read = open(file_name, 'r')
+    last_seen_id = int(f_read.read().strip())
+    f_read.close()
+    return last_seen_id
+
+
+def store_last_seen_id(last_seen_id, file_name):
+    f_write = open(file_name, 'w')
+    f_write.write(str(last_seen_id))
+    f_write.close()
+    return
+
+
+def reply_to_tweets():
+    print('retrieving and replying to tweets...')
+    # DEV NOTE: use my second account for testing.
+    last_seen_id = retrieve_last_seen_id(FILE_NAME)
+    # NOTE: We need to use tweet_mode='extended' below to show
+    # all full tweets (with full_text). Without it, long tweets
+    # would be cut off.
+    mentions = api.mentions_timeline(last_seen_id, tweet_mode='extended')
+    for mention in reversed(mentions):
+        print(str(mention.id) + ' - ' + mention.full_text)
+        last_seen_id = mention.id
+        store_last_seen_id(last_seen_id, FILE_NAME)
+        if '#helloworld' in mention.full_text.lower():
+            print('found #helloworld!')
+            print('responding back...')
+            api.update_status('@' + mention.user.screen_name +
+                              '#HelloWorld Test_Answer', mention.id)
+
+
+while True:
+    reply_to_tweets()
+    time.sleep(15)
